4e95e44c4862c89eb29c7a1c785f0d9f072a0cf8
Ефименко Александра Сергеевна <efimenko.2018@stud.nstu.ru>
2023-11-22 20:29:33 +0700

    Саша сделала свою часть.

---
+++ РГЗ-фронт/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace РГЗ_фронт
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Image> photos = new ObservableCollection<Image>();
+
+        public void AddPhoto(string base64string)
+        {
+            byte[] binaryData = Convert.FromBase64String(base64string);
+
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.StreamSource = new MemoryStream(binaryData);
+            bi.EndInit();
+
+            Image img = new Image();
+            img.Source = bi;
+
+            photos.Add(img);
+        }
+
+        private void ClearPhotos()
+        {
+            photos.Clear();
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ViewerPhotos.ItemsSource = photos;
+        }
+
+        private void GetPhotos_Click(object sender, RoutedEventArgs e)
+        {
+            ClearPhotos();
+
+            // Получение фотографий с сервера
+            string path = Path.Text;    // Получение пути к папке с фотографиями
+            string[] base64Strings = { }; // TODO: get photo src base64;
+
+            // Запись фотографий
+            foreach(string base64String in base64Strings)
+            {
+                AddPhoto(base64String);
+            }
+        }
+    }
+}
